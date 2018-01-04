18d33033ef442676b152773cd44cbb34d8ac20ea
Ruben Romero Montes <rromerom@redhat.com>
2018-01-04 15:28:45 +0100

    fixed token path

--- manager-app/server/models/oc.js
+++ manager-app/server/models/oc.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 function loadToken(callbackFn) {
   try {
-    return fs.readFileSync('/run/secrets/kubernetes.io/token', 'utf8');
+    return fs.readFileSync('/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
   } catch (error) {
     throw new Error('Missing required TOKEN. Check that the ServiceAccount is properly defined');
   }
