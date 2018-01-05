f466f8d17205a040f985c3fdaf1d0103a35146d3
Ruben Romero Montes <rromerom@redhat.com>
2018-01-05 15:44:54 +0100

    changed path

--- manager-app/server/models/validator.js
+++ manager-app/server/models/validator.js
@@ -4,14 +4,13 @@ const OC = require('./oc');
 const ROUTE = 'http://' + OC.getDemoAppRoute();
 
 exports.check = function(level, callbackFn) {
-  http.get(ROUTE, (resp) => {
+  http.get(ROUTE + '/index.json', (resp) => {
     let body = "";
     resp.on('data', data => {
       body += data;
     });
     resp.on("end", () => {
       body = JSON.parse(body);
-      console.log(body);
       callbackFn(body.level === level);
     });
   });
