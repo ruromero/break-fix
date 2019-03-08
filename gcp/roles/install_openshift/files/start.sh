649f3f4e47cfcf06dfb2c9abd2d406043b0dd913
Juan Manuel Parrilla <jparrill@redhat.com>
2019-03-08 18:18:30 +0100

    Adding updates for new updates on 3.11 (#26)
    * Fixig addon url
    
    * Added fixes to make this work properly

--- gcp/roles/install_openshift/files/start.sh
+++ gcp/roles/install_openshift/files/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo oc cluster up --public-hostname=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip) --routing-suffix=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip).nip.io
 sudo oc login -u system:admin
-sudo oc adm policy add-role-to-user admin developer -n demo
